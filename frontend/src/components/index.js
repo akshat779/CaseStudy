3934e411c9b803250bfec433fccd9c1a0c482906
Akshat Sharma <akshatsharma779@gmail.com>
2025-04-08 11:54:45 +0530

    done maybe

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -3,19 +3,16 @@ import { CheckBox } from "./CheckBox";
 import { Heading } from "./Heading";
 import { Img } from "./Img";
 import { Input } from "./Input";
-import { RatingBar } from "./RatingBar";
 import { SelectBox } from "./SelectBox";
-import { Slider } from "./Slider";
-import { Switch } from "./Switch";
-import { Text } from "./Text";export {
+import { Text } from "./Text";
+
+
+export {
   Button,
   CheckBox,
   Heading,
   Img,
   Input,
-  RatingBar,
   SelectBox,
-  Slider,
-  Switch,
   Text,
 };
